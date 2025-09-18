300a279eec4ed89886d22688d9e42284485f8841
Kaiz Dawson <kaizdawson@gmail.com>
2025-09-19 01:23:43 +0700

    swagger v1

--- AESP.API/Program.cs
+++ AESP.API/Program.cs
@@ -112,8 +112,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AESP.API v1");
+        c.RoutePrefix = "swagger"; 
+    });
 }
+
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
