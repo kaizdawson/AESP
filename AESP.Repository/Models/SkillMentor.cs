9f79081f96088d184936353a916a35391ed6d763
Tuan <tuan0@DESKTOP-TF9MPQP>
2025-09-25 19:49:33 +0700

    addTblRoomAndSkillAndSkillMentor

---
+++ AESP.Repository/Models/SkillMentor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESP.Repository.Models
+{
+    public class SkillMentor
+    {
+        [Key]
+        public Guid SkillMentorId { get; set; }
+
+        public Guid MentorProfileId { get; set; }
+        [ForeignKey(nameof(MentorProfileId))]
+        public MentorProfile MentorProfile { get; set; } = null!;
+
+        public Guid SkillId { get; set; }
+        [ForeignKey(nameof(SkillId))]
+        public Skill Skill { get; set; } = null!;
+
+        public bool IsExpertised { get; set; } = false;
+    }
+}
