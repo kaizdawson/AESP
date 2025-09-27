596d745d6881c044894ce5b6052b76817030283b
Kaiz Dawson <kaizdawson@gmail.com>
2025-09-27 23:27:24 +0700

    thêm roleName khi login

--- AESP.Common/DTOs/LoginResult.cs
+++ AESP.Common/DTOs/LoginResult.cs
@@ -5,5 +5,7 @@
         public bool Success { get; set; }
         public string Message { get; set; } = string.Empty;
         public string? Token { get; set; }
+
+        public string? RoleName { get; set; }
     }
 }
