bc90844bff8ec2175fa8a0850fa4830828e084eb
Kaiz Dawson <kaizdawson@gmail.com>
2025-09-28 17:01:44 +0700

    fix valid

--- AESP.Common/DTOs/SignInDto.cs
+++ AESP.Common/DTOs/SignInDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace AESP.Common.DTOs
 {
     public class SignInDto
     {
+        [Required(ErrorMessage = "Số điện thoại không được để trống.")]
+        [RegularExpression(@"^0\d{9}$", ErrorMessage = "Số điện thoại phải có 10 chữ số và bắt đầu bằng 0.")]
         public string PhoneNumber { get; set; } = null!;
+        [Required(ErrorMessage = "Mật khẩu không được để trống.")]
         public string Password { get; set; } = null!;
     }
 }
