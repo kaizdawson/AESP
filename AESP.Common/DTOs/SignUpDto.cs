010a18915c165eb1ebeebb56a85202a5237076d7
Kaiz Dawson <kaizdawson@gmail.com>
2025-10-04 15:35:26 +0700

    fix valid + role

--- AESP.Common/DTOs/SignUpDto.cs
+++ AESP.Common/DTOs/SignUpDto.cs
@@ -1,6 +1,6 @@
-﻿using AESP.Common.Enums;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -25,12 +25,13 @@ namespace AESP.Common.DTOs
 
 
         [Required(ErrorMessage = "Mật khẩu không được để trống.")]
-        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,10}$",
-            ErrorMessage = "Mật khẩu phải từ 6-10 ký tự, có ít nhất 1 chữ hoa, 1 số và 1 ký tự đặc biệt.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$",ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự, có ít nhất 1 chữ hoa, 1 số và 1 ký tự đặc biệt.")]
+        [DefaultValue("Abc@123")]
         public string Password { get; set; } = null!;
 
+
         [Required(ErrorMessage = "Role không được để trống.")]
-        [JsonConverter(typeof(JsonStringEnumConverter))]
-        public UserRole Role { get; set; }
+        [DefaultValue("LEARNER")]
+        public string Role { get; set; } = null!;
     }
 }
