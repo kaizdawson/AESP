560b7a0fc8bd7d8eb917f07d4a89af2a2683ef20
NguyenToan234 <nguyenvantoanpr359@gmail.com>
2025-10-08 00:23:24 +0700

    CRUD Course + fix role Admin Migration

---
+++ AESP.Repository/Migrations/20251007110544_SeedAdminAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AESP.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdminAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "AvatarUrl", "Email", "FirebaseUid", "FullName", "PasswordHash", "PhoneNumber", "Role", "Status" },
+                values: new object[] { new Guid("11111111-1111-1111-1111-111111111111"), "", "admin@aesp.com", "", "Super Admin", "6G94qKPK8LYNjnTllCqm2G3BUM08AzOK7yW30tfjrMc=", "0909000000", "Admin", "Active" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"));
+        }
+    }
+}
