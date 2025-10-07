b3d5efb00e3a545f542add0c6b9dbb289c417256
Tuan <tuan0@DESKTOP-TF9MPQP>
2025-10-08 01:04:05 +0700

    FixValidManager

--- AESP.API/Program.cs
+++ AESP.API/Program.cs
@@ -80,10 +80,9 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
 builder.Services.AddScoped<IQuestionAssessmentService, QuestionAssessmentService>();
 
-<<<<<<< HEAD
-=======
+
 builder.Services.AddScoped<ICourseService, CourseService>();
->>>>>>> master
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
