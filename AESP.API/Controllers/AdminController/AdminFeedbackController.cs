b88af88a2483fb2bb9a514b90fdf01849eb17865
TuanETV123 <tuanetv123@gmail.com>
2025-10-19 02:20:11 +0700

    done DashboardAdmin and loginGoogle role reviewer

---
+++ AESP.API/Controllers/AdminController/AdminFeedbackController.cs
@@ -0,0 +1,36 @@
+﻿using AESP.Service.Contract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AESP.API.Controllers.AdminController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "ADMIN")]
+    public class AdminFeedbackController : ControllerBase
+    {
+        private readonly IAdminFeedbackService _service;
+
+        public AdminFeedbackController(IAdminFeedbackService service)
+        {
+            _service = service;
+        }
+
+        //  Lấy danh sách Feedback
+        [HttpGet]
+        public async Task<IActionResult> GetAllFeedback()
+        {
+            var result = await _service.GetAllFeedbackAsync();
+            return Ok(result);
+        }
+
+        //  Lấy chi tiết Feedback theo ID
+        [HttpGet("{feedbackId}")]
+        public async Task<IActionResult> GetFeedbackDetail(Guid feedbackId)
+        {
+            var result = await _service.GetFeedbackDetailAsync(feedbackId);
+            return Ok(result);
+        }
+    }
+}
---
+++ AESP.Service/Contract/IAdminDashboardService.cs
@@ -0,0 +1,17 @@
+﻿using AESP.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESP.Service.Contract
+{
+    public interface IAdminDashboardService
+    {
+        Task<ResponseDTO> GetSummaryAsync();
+        Task<ResponseDTO> GetPackagesByMonthAsync(int year);
+        Task<ResponseDTO> GetRevenueByMonthAsync(int year);
+        Task<ResponseDTO> GetPendingReviewersAsync(int pageNumber, int pageSize);
+    }
+}
