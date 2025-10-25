65125bc1924371e9c3b2f3154c7ca10c500ad8d3
Kaiz Dawson <kaizdawson@gmail.com>
2025-10-25 12:39:44 +0700

    fix getMe 3 layer

---
+++ AESP.Service/Contract/IAuthQueryService.cs
@@ -0,0 +1,14 @@
+﻿using AESP.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESP.Service.Contract
+{
+    public interface IAuthQueryService
+    {
+        Task<ResponseDTO> GetUserInfoAsync(Guid userId);
+    }
+}
