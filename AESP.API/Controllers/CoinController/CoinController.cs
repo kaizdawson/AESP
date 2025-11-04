7ce65498e65e0245f10ccf5d51b7c7600991d975
Kaiz Dawson <kaizdawson@gmail.com>
2025-11-04 15:12:37 +0700

    PayOs

---
+++ AESP.API/Controllers/CoinController/CoinController.cs
@@ -0,0 +1,90 @@
+﻿using AESP.Common.DTOs;
+using AESP.Service.Contract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AESP.API.Controllers.CoinController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CoinController : ControllerBase
+    {
+        private readonly ICoinService _coinService;
+
+        public CoinController(ICoinService coinService)
+        {
+            _coinService = coinService;
+        }
+
+        [HttpGet("balance")]
+        public async Task<IActionResult> GetMyCoinBalance()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userIdClaim))
+                return Unauthorized("Access token không hợp lệ hoặc thiếu UserId.");
+
+            if (!Guid.TryParse(userIdClaim, out var userId))
+                return BadRequest("UserId trong token không hợp lệ.");
+
+            try
+            {
+                var balance = await _coinService.GetUserCoinBalanceAsync(userId);
+                return Ok(new { coinBalance = balance });
+            }
+            catch
+            {
+                return BadRequest(new { message = "Không thể lấy thông tin coin balance." });
+            }
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> AddCoin([FromBody] AddCoinRequest request)
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(userIdClaim))
+                return Unauthorized("Access token không hợp lệ hoặc thiếu UserId.");
+
+            if (!Guid.TryParse(userIdClaim, out var userId))
+                return BadRequest("UserId trong token không hợp lệ.");
+
+            try
+            {
+                var checkoutUrl = await _coinService.AddCoinAsync(request.ServicePackageId, userId);
+                return Ok(new { checkoutUrl });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("cancel")]
+        public async Task<IActionResult> CancelTransaction([FromBody] string orderCode)
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(userIdClaim))
+                return Unauthorized("Access token không hợp lệ hoặc thiếu UserId.");
+
+            if (!Guid.TryParse(userIdClaim, out var userId))
+                return BadRequest("UserId trong token không hợp lệ.");
+
+            try
+            {
+                await _coinService.CancelTransactionAsync(userId, orderCode);
+                return Ok(new { message = "Giao dịch đã hủy thành công." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+
+
+    }
+}
---
+++ AESP.Common/DTOs/AddCoinRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESP.Common.DTOs
+{
+    public class AddCoinRequest
+    {
+        public Guid ServicePackageId { get; set; }
+    }
+
+}
