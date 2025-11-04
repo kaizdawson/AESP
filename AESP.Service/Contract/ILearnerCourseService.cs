8891fef86e30c9205ea06c296cef707d1a5a0e70
NguyenToan234 <nguyenvantoanpr359@gmail.com>
2025-11-04 17:14:39 +0700

    GetCourse+EnrollCourse For Learner

---
+++ AESP.Service/Contract/ILearnerCourseService.cs
@@ -0,0 +1,16 @@
+﻿using AESP.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESP.Service.Contract
+{
+    public interface ILearnerCourseService
+    {
+        Task<ResponseDTO> EnrollAsync(Guid learnerId, Guid courseId);
+        Task<ResponseDTO> UnenrollAsync(Guid learnerId, Guid courseId);
+        Task<ResponseDTO> UpdateProgressAsync(Guid learnerId, Guid courseId, double progress);
+    }
+}
