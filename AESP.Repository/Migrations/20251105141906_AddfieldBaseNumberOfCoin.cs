07b12516205a931fbd70906dc57c719786431b87
TuanETV123 <tuanetv123@gmail.com>
2025-11-05 22:25:03 +0700

    AddMigration new field ServicePackage and fix logic

---
+++ AESP.Repository/Migrations/20251105141906_AddfieldBaseNumberOfCoin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AESP.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddfieldBaseNumberOfCoin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BaseNumberOfCoin",
+                table: "ServicePackages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 5, 14, 19, 6, 43, DateTimeKind.Utc).AddTicks(1881));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BaseNumberOfCoin",
+                table: "ServicePackages");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 4, 17, 40, 1, 483, DateTimeKind.Utc).AddTicks(2427));
+        }
+    }
+}
