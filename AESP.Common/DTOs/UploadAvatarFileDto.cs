c9463205c54f52a1f836fde270be893116bc4482
Tuan <tuan0@DESKTOP-TF9MPQP>
2025-11-10 02:19:40 +0700

    fix AdminLearner new logic and Done Task Upload AvatarUser

---
+++ AESP.Common/DTOs/UploadAvatarFileDto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESP.Common.DTOs
+{
+    public class UploadAvatarFileDto
+    {
+        [Required]
+        [FromForm(Name = "file")]
+        [SwaggerSchema("Ảnh đại diện (JPG, PNG, WEBP, ...)")]
+        public IFormFile File { get; set; } = null!;
+    }
+}
