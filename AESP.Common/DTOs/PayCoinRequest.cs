fdfb0a14052c0bc0aac554c4fe840836861189c3
Kaiz Dawson <kaizdawson@gmail.com>
2025-11-13 18:08:57 +0700

    paycoin api

---
+++ AESP.Common/DTOs/PayCoinRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESP.Common.DTOs
+{
+    public class PayCoinRequest
+    {
+        public int PayCoin { get; set; }
+    }
+}
