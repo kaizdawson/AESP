f9f1d395c54b3f5882a2f9226ac2e4c9afbea6b1
Kaiz Dawson <kaizdawson@gmail.com>
2025-11-13 23:02:32 +0700

    withdrawal

---
+++ AESP.Common/DTOs/WithdrawRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESP.Common.DTOs
+{
+    public class WithdrawRequest
+    {
+        public int Coin { get; set; }
+        public string BankName { get; set; }
+        public string AccountNumber { get; set; }
+    }
+
+}
