958a29085ea0a23685b7cc960f65b9f120cc6166
TuanETV123 <tuanetv123@gmail.com>
2025-11-23 03:56:05 +0700

    Done task GetLearnerHistoryReview

---
+++ AESP.API/Controllers/LearnerController/LearnerReviewController.cs
@@ -0,0 +1,58 @@
+﻿using AESP.Repository.DB;
+using AESP.Repository.Models;
+using AESP.Service.Contract;
+using AESP.Service.Implementation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AESP.API.Controllers.LearnerController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "LEARNER")]
+    public class LearnerReviewController : ControllerBase
+    {
+        private readonly ILearnerBuyReview _service;
+
+        public LearnerReviewController(ILearnerBuyReview service)
+        {
+            _service = service;
+        }
+        [HttpGet("my-history")]
+        public async Task<IActionResult> GetLearnerReviewHistory(
+       [FromQuery] int pageNumber = 1,
+       [FromQuery] int pageSize = 10)
+        {
+            var learnerProfileId = GetLearnerProfileIdFromToken(User);
+
+            if (learnerProfileId == null)
+                return Unauthorized(new { message = "Không xác định được learner từ token." });
+
+            var result = await _service.GetLearnerReviewHistoryAsync(
+                learnerProfileId.Value, pageNumber, pageSize);
+
+            return StatusCode(result.IsSucess ? 200 : 400, result);
+        }
+        private Guid? GetLearnerProfileIdFromToken(ClaimsPrincipal user)
+        {
+            var userIdClaim = user.FindFirst(ClaimTypes.NameIdentifier)?.Value ??
+                              user.FindFirst("sub")?.Value;
+
+            if (string.IsNullOrEmpty(userIdClaim))
+                return null;
+
+            if (!Guid.TryParse(userIdClaim, out var userId))
+                return null;
+
+            var db = HttpContext.RequestServices.GetService<AppDbContext>();
+            var learner = db.Set<LearnerProfile>()
+                            .FirstOrDefault(r => r.UserId == userId);
+
+            return learner?.LearnerProfileId;
+        }
+
+
+    }
+}
