177d0fcd41334e5ccd85ee8a192cefeb654af9c7
Kaiz Dawson <kaizdawson@gmail.com>
2025-12-21 14:33:24 +0700

    fix record

--- AESP.Common/DTOs/SubmitRecordDTO.cs
+++ AESP.Common/DTOs/SubmitRecordDTO.cs
@@ -9,7 +9,7 @@ namespace AESP.Common.DTOs
     public class SubmitRecordDTO
     {
         public string AudioRecordingURL { get; set; } = string.Empty;
-        public string Content { get; set; } = string.Empty;
+        public string TranscribedText { get; set; }
         public double Score { get; set; }
         public string AIFeedback { get; set; } = string.Empty;
     }
